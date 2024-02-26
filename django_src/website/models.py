b8d2d0b7d1e36c4e6a094d4f455e2e092bc6f10e
Marouane Soukhrati <83184715+MarouaneSoukhrati@users.noreply.github.com>
2024-02-26 18:57:51 +0100

    django trash

--- django_src/website/models.py
+++ django_src/website/models.py
@@ -1,13 +1,8 @@
 from django.db import models
 
 class Bloc(models.Model):
-    title = models.CharField(max_length="60")
-    content = models.TextField()
+    number = models.IntegerField()
+    name = models.TextField()
     
     def __str__(self):
-        return self.title
-    
-class Page(models.Model):
-    title = models.CharField(max_length="50")
-    content = models.TextField()
-    main_bloc = models.ForeignKey(Bloc, on_delete=models.CASCADE) 
+        return self.name
