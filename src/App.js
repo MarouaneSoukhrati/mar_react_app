e873e851beccd48f95ab32d5bcfd11b541ec66ef
Marouane Soukhrati <83184715+MarouaneSoukhrati@users.noreply.github.com>
2024-02-26 15:21:21 +0100

    update last v

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Game from './Addons/tictactoe';
-import Mybox from './Addons/mylist';
+import Mylister from './Addons/mylist';
 
 function App() {
   return (
@@ -20,7 +20,7 @@ function App() {
           Learn To code
         </a>
         <Game/>
-        <Mybox/>
+        <Mylister/>
       </header>
     </div>
   );
