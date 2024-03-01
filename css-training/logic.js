e84d5f52256cab59a6deb396807f0c0d8600fffb
Marouane Soukhrati <83184715+MarouaneSoukhrati@users.noreply.github.com>
2024-03-01 11:04:29 +0000

    tkharbi9

---
+++ css-training/logic.js
@@ -0,0 +1,15 @@
+function change_content(){
+    let title = document.getElementsByClassName("title")[0];
+    title.innerHTML = title.innerHTML === "First title" ? "Second title" : "First title";
+}
+
+Links = [
+    "https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_media_queries",
+    "https://developer.mozilla.org/fr/docs/Web/CSS/position",
+    "https://developer.mozilla.org/fr/docs/Web/CSS/display",
+    "https://developer.mozilla.org/fr/docs/Web/CSS/float",
+    "https://developer.mozilla.org/fr/docs/Web/CSS/clear",
+    "https://developer.mozilla.org/fr/docs/Web/HTML/Element/caption",
+    "https://developer.mozilla.org/en-US/docs/Web/HTML/Viewport_meta_tag",
+    "web.dev"
+];
