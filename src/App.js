5dfc9a557f5be9127220186f2fd278c6ea0082eb
Marouane Soukhrati <83184715+MarouaneSoukhrati@users.noreply.github.com>
2024-03-25 08:25:13 +0000

    background for router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import "./App.css";
-import Aheader from './Component/Aheader';
-import Abody from './Component/Abody';
-import Afooter from './Component/Afooter';
+import Aheader from "./Component/Aheader";
+import Abody from "./Component/Abody";
+import Afooter from "./Component/Afooter";
+
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+import FamilyTree from "./Component/Subcomponent/FamilyTree";
 
 function App() {
   return (
     <div className="App">
-      <Aheader/>
-      <Abody/>
-      <Afooter/>
+      <Aheader />
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Abody />} />
+          <Route exact path="/about" element={<FamilyTree />} />
+        </Routes>
+      </Router>
+      <Afooter />
     </div>
   );
 }
