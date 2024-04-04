0e08a5006767399bf68d756a663450cbef9eb93e
Marouane Soukhrati <marouane.soukhrati@uha.fr>
2024-04-04 19:48:18 +0200

    sir t7awa

--- src/Component/Subcomponent/Navbar.js
+++ src/Component/Subcomponent/Navbar.js
@@ -4,70 +4,43 @@ import UkFlagLogo from "../../Logos/UkFlag.svg";
 import FrFlagLogo from "../../Logos/FrFlag.svg";
 import EsFlagLogo from "../../Logos/EsFlag.svg";
 
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { useState } from "react";
 
 export default function Navbar() {
-  const [indexList, setIndexList] = useState([1, 0, 0, 0, 1, 0, 0]);
+  const [indexList, setIndexList] = useState([1, 0, 0]);
   let activeNav = function (index) {
-    if (index < 4) {
-      return indexList[index] ? "nav-element-active" : "nav-element";
-    }
     return indexList[index] ? "lang-active" : "lang";
   };
 
   return (
     <div className="big-navbar">
       <div className="navbar">
-        <Link className={activeNav(0)} to="/">
-          <div
-            onClick={() => setIndexList([1, 0, 0, 0, ...indexList.slice(4)])}
-          >
-            Home
-          </div>
-        </Link>
-        <Link className={activeNav(1)} to="/portfolio">
-          <div
-            onClick={() => setIndexList([0, 1, 0, 0, ...indexList.slice(4)])}
-          >
-            Portfolio
-          </div>
-        </Link>
-        <Link className={activeNav(2)} to="/about">
-          <div
-            onClick={() => setIndexList([0, 0, 1, 0, ...indexList.slice(4)])}
-          >
-            About
-          </div>
-        </Link>
-        <Link className={activeNav(3)} to="/contact">
-          <div
-            onClick={() => setIndexList([0, 0, 0, 1, ...indexList.slice(4)])}
-          >
-            Contact
-          </div>
-        </Link>
+        <NavLink className="nav-element" to="/"><div>Home</div></NavLink>
+        <NavLink className="nav-element" to="/portfolio"><div>Portfolio</div></NavLink>
+        <NavLink className="nav-element" to="/about"><div>About</div></NavLink>
+        <NavLink className="nav-element" to="/contact"><div>Contact</div></NavLink>
       </div>
       <div className="langbar">
         <img
           src={UkFlagLogo}
-          className={activeNav(4)}
+          className={activeNav(0)}
           alt="UkFlag"
-          onClick={() => setIndexList([...indexList.slice(0, 4), 1, 0, 0])}
+          onClick={() => setIndexList([1, 0, 0])}
         />{" "}
         EN
         <img
           src={FrFlagLogo}
-          className={activeNav(5)}
+          className={activeNav(1)}
           alt="FrFlag"
-          onClick={() => setIndexList([...indexList.slice(0, 4), 0, 1, 0])}
+          onClick={() => setIndexList([0, 1, 0])}
         />{" "}
         FR
         <img
           src={EsFlagLogo}
-          className={activeNav(6)}
+          className={activeNav(2)}
           alt="EsFlag"
-          onClick={() => setIndexList([...indexList.slice(0, 4), 0, 0, 1])}
+          onClick={() => setIndexList([0, 0, 1])}
         />{" "}
         ES
       </div>
