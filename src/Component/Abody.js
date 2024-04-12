675ad956aa4bbd9b3f3dec8debc53deb5b1457ed
Marouane Soukhrati <marouane.soukhrati@uha.fr>
2024-04-12 21:07:22 +0200

    cv

--- src/Component/Abody.js
+++ src/Component/Abody.js
@@ -10,53 +10,77 @@ aboutPara.push(" In short, I like exploring ways to enhance computers so that th
 aboutPara.push(" I'm also an engineering student at ENSISA who is currently looking for a co-op position");
 aboutPara.push(" I'm from Tangier but I have setteled in Mulhouse for the moment.");
 
+let ExpList = [
+  {corp:"Mécanique Baumoise de Précision", date:"2020-2022", title:"Software Engineer", 
+    job:"Responsible for developing a user-friendly reporting application to deal with 3D reports for an optimised usage of in-house metrology software."},
+  {corp:"Daher Aerospace", date:"2019", title:"Intern", job:"Discovery Internship"},
+  {corp:"Freelance", date:"2017", title:"Freelancer", job:"Mathematics tutor/IT"},
+];
+
+let EduList = [
+  {school:"Ecole Nationale Supérieure d’Electricité et de Mécanique (ENSEM)", loc: "Nancy", date:"2020-2022", description:"Energy systems engineering"},
+  {school:"ENSEIRB-MATMECA", loc: "Bordeaux", date:"2018-2019", description:"Software Engineer"},
+  {school:"CPGE Saint-Exupéry", loc: "Mantes-La-Jolie", date:"2017-2018", description:"Undergraduate studies to prepare for competitive entry exams to engineering schools"},
+];
+
 export default function Abody() {
   
-  const experienceList = [1,2,3,4].map(e => { 
-    return <div>
-            <div style={{display: "flex", flexDirection: "row", justifyContent:"space-between", width:"120px"}}><img src={FrFlagLogo} style={{width:"20px"}} alt="img-cv"/><span>Design</span><span>Code</span></div>
-            <h1>Senior</h1>
-            <p>In short, I like exploring ways to enhance computers so that they can better serve human creativity and prosperity</p>
-          </div>})
+  const experienceList = ExpList.map(e => { 
+    return <>
+            <div className="Exp-List"><img src={FrFlagLogo} style={{width:"20px", margin:"0px 10px 0px 0px"}} alt="img-cv"/> {e.corp} {e.date}</div>
+            <h1>{e.title}</h1>
+            <p>{e.job}</p>
+          </>})
 
-  const skillsListLeft = [1,2,3,4].map(e => { 
-    return <li>Code</li>})      
+  const educationList = EduList.map(e => { 
+    return <>
+            <div className="Edu-List">{e.loc} {e.date}</div>
+            <h2 style={{textAlign:"center"}}>{e.school}</h2>
+            <div style={{textAlign:"center", marginBottom:"50px"}}>{e.description}</div>
+         </>})           
+
+  const skillsListLeft = <>
+    <li>Web development</li>
+    <li>Web Design</li>
+    <li>Data analytics</li>
+    <li>Marketing</li>
+  </>     
   
-  const skillsListRight = [1,2,3,4].map(e => { 
-    return <li>Design</li>})     
-
-  const educationList = [1,2,3,4].map(e => { 
-    return <div>
-              <div style={{display: "flex", flexDirection: "row", justifyContent:"space-between", width:"120px"}}><span>2021</span><span>Present</span></div>
-              <h2>MIT - Massachusetts Institute of Technology</h2>
-              <h3>Engineering</h3>
-           </div>})      
+  const skillsListRight = <>
+    <li>IT/Software developement</li>
+    <li>Computation</li>
+    <li>Energy systems design</li>
+    <li>Complex Energy systems maintenance</li>
+  </>           
 
   return (
     <div className="App-body">
-      <div className="body-left">
-        <img src={marouaneLogo} alt="img-cv"/>
-        <h1>Marouane Soukhrati</h1>
-        <h2>Computer Science Student</h2>
-        <div><img src={FrFlagLogo} style={{width:"20px"}} alt="img-cv"/> soukhratimarouane@gmail.com</div>
-        <div><img src={FrFlagLogo} style={{width:"20px"}} alt="img-cv"/> +33667569678</div>
-        <h1>About Me</h1>
-        <p style={{textAlign:"center", width:"700px"}}>{aboutPara}</p>
-        <h1>Skills</h1>
-        <div className="skills-area">
-          <ul>
-            {skillsListLeft}
-          </ul>
-          <ul>
-            {skillsListRight}
-          </ul>
+      <h1 style={{padding:"50px"}}>Curriculum Vitae</h1>
+      <div className="body-wrapper">
+        <div className="body-left">
+          <img src={marouaneLogo} alt="img-cv"/>
+          <h1 style={{margin: "90px 0px 60px 0px"}}>Marouane Soukhrati</h1>
+          <h3>Computer Science Student</h3>
+          <div><img src={FrFlagLogo} style={{width:"20px"}} alt="img-cv"/> soukhratimarouane@gmail.com</div>
+          <div><img src={FrFlagLogo} style={{width:"20px"}} alt="img-cv"/> +33667569678</div>
+          <h1 style={{margin: "90px 0px 30px 0px"}}>About Me</h1>
+          <p style={{textAlign:"center", width:"645px"}}>{aboutPara}</p>
+          <h1 style={{margin: "90px 0px 30px 0px"}}>Skills</h1>
+          <div className="skills-area">
+            <ul>
+              {skillsListLeft}
+            </ul>
+            <ul>
+              {skillsListRight}
+            </ul>
+          </div>
         </div>
-      </div>
-      <div className="body-right">
-        <h1>Past Experience</h1>
-        {experienceList}
-        <h1>Education</h1>
-        {educationList}
+        <div className="body-right">
+          <h1 style={{margin: "50px 0px 90px 0px"}}>Past Experience</h1>
+          <div>{experienceList}</div>
+          <h1 style={{margin: "10px 0px 70px 0px"}}>Education</h1>
+          <div style={{display:"flex", flexDirection: "column", alignItems:"center"}}>{educationList}</div>
+        </div>
       </div>
     </div>
   );
