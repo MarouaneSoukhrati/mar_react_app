d1aa0f6597182787756b80b83f8b36773d802db8
Marouane Soukhrati <marouane.soukhrati@uha.fr>
2024-04-13 10:32:24 +0200

    cv ud

--- src/Component/Abody.js
+++ src/Component/Abody.js
@@ -1,7 +1,11 @@
 import "../ComponentStyle/Abody.css";
 
 import marouaneLogo from "../Logos/marouane-logo.svg";
-import FrFlagLogo from "../Logos/FrFlag.svg";
+import gmailLogo from "../Logos/gmailLogo.svg";
+import phoneLogo from "../Logos/phoneLogo.svg";
+import mbpLogo from "../Logos/mbpLogo.svg";
+import daherLogo from "../Logos/daherLogo.svg";
+import freelanceLogo from "../Logos/freelanceLogo.svg";
 
 let aboutPara = ["I am interested in understanding and articulating everything via code."];
 aboutPara.push(" Whatever be the problem, if it involves challenges in formally representing it and its associated metrics, count me in.");
@@ -11,13 +15,14 @@ aboutPara.push(" I'm also an engineering student at ENSISA who is currently look
 aboutPara.push(" I'm from Tangier but I have setteled in Mulhouse for the moment.");
 
 let ExpList = [
-  {corp:"Mécanique Baumoise de Précision", date:"2020-2022", title:"Software Engineer", 
+  {corp:"Mécanique Baumoise de Précision", logo : mbpLogo, date:"2020-2022", title:"Software Engineer", 
     job:"Responsible for developing a user-friendly reporting application to deal with 3D reports for an optimised usage of in-house metrology software."},
-  {corp:"Daher Aerospace", date:"2019", title:"Intern", job:"Discovery Internship"},
-  {corp:"Freelance", date:"2017", title:"Freelancer", job:"Mathematics tutor/IT"},
+  {corp:"Daher Aerospace", logo : daherLogo, date:"2019", title:"Intern", job:"Discovery Internship"},
+  {corp:"Freelance", logo : freelanceLogo, date:"2017", title:"Freelance Tutor", job:"Mathematics tutoring"},
 ];
 
 let EduList = [
+  {school:"École nationale supérieure d'ingénieurs Sud-Alsace (ENSISA)", loc: "Mulhouse", date:"2023-2027", description:"Computer Science"},
   {school:"Ecole Nationale Supérieure d’Electricité et de Mécanique (ENSEM)", loc: "Nancy", date:"2020-2022", description:"Energy systems engineering"},
   {school:"ENSEIRB-MATMECA", loc: "Bordeaux", date:"2018-2019", description:"Software Engineer"},
   {school:"CPGE Saint-Exupéry", loc: "Mantes-La-Jolie", date:"2017-2018", description:"Undergraduate studies to prepare for competitive entry exams to engineering schools"},
@@ -27,15 +32,15 @@ export default function Abody() {
   
   const experienceList = ExpList.map(e => { 
     return <>
-            <div className="Exp-List"><img src={FrFlagLogo} style={{width:"20px", margin:"0px 10px 0px 0px"}} alt="img-cv"/> {e.corp} {e.date}</div>
-            <h1>{e.title}</h1>
+            <h3 className="Exp-List"><img src={e.logo} style={{width:"40px", margin:"0px 10px 0px 0px"}} alt="img-cv"/> {e.corp + " - "} {e.date}</h3>
+            <h5>{e.title}</h5>
             <p>{e.job}</p>
           </>})
 
   const educationList = EduList.map(e => { 
     return <>
-            <div className="Edu-List">{e.loc} {e.date}</div>
             <h2 style={{textAlign:"center"}}>{e.school}</h2>
+            <div className="Edu-List">{e.loc} {e.date}</div>
             <div style={{textAlign:"center", marginBottom:"50px"}}>{e.description}</div>
          </>})           
 
@@ -61,8 +66,8 @@ export default function Abody() {
           <img src={marouaneLogo} alt="img-cv"/>
           <h1 style={{margin: "90px 0px 60px 0px"}}>Marouane Soukhrati</h1>
           <h3>Computer Science Student</h3>
-          <div><img src={FrFlagLogo} style={{width:"20px"}} alt="img-cv"/> soukhratimarouane@gmail.com</div>
-          <div><img src={FrFlagLogo} style={{width:"20px"}} alt="img-cv"/> +33667569678</div>
+          <div style={{marginBottom:"10px"}}><img src={gmailLogo} style={{width:"20px"}} alt="img-cv"/> soukhratimarouane@gmail.com</div>
+          <div><img src={phoneLogo} style={{width:"20px", margin:"-4px 0px"}} alt="img-cv"/> +33667569678</div>
           <h1 style={{margin: "90px 0px 30px 0px"}}>About Me</h1>
           <p style={{textAlign:"center", width:"645px"}}>{aboutPara}</p>
           <h1 style={{margin: "90px 0px 30px 0px"}}>Skills</h1>
@@ -78,7 +83,7 @@ export default function Abody() {
         <div className="body-right">
           <h1 style={{margin: "50px 0px 90px 0px"}}>Past Experience</h1>
           <div>{experienceList}</div>
-          <h1 style={{margin: "10px 0px 70px 0px"}}>Education</h1>
+          <h1 style={{margin: "30px 0px 50px 0px"}}>Education</h1>
           <div style={{display:"flex", flexDirection: "column", alignItems:"center"}}>{educationList}</div>
         </div>
       </div>
