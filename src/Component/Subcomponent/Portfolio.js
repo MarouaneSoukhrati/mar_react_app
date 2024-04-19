937618cdaa9219bd7b6f6f42308cd04173ad923d
Marouane Soukhrati <83184715+MarouaneSoukhrati@users.noreply.github.com>
2024-04-19 13:25:03 +0000

    audio player

--- src/Component/Subcomponent/Portfolio.js
+++ src/Component/Subcomponent/Portfolio.js
@@ -1,9 +1,11 @@
 import "../../ComponentStyle/SubcomponentStyle/Portfolio.css";
+import AudioPlayer from "./AudioPlayer";
 
 export default function Portfolio() {
   return (
     <header className="App-portfolio">
       <h1>Portfolio</h1>
+      <AudioPlayer />
     </header>
   );
 }
