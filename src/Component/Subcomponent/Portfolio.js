4ef60956d31ad398ed7bf73f1946da8b14e63dc4
Marouane Soukhrati <83184715+MarouaneSoukhrati@users.noreply.github.com>
2024-05-18 14:12:26 +0000

    prompt

--- src/Component/Subcomponent/Portfolio.js
+++ src/Component/Subcomponent/Portfolio.js
@@ -1,9 +1,44 @@
 import "../../ComponentStyle/SubcomponentStyle/Portfolio.css";
+import { useState } from "react";
+
+import rightArrow from "../../Logos/rightArrow.svg";
 
 export default function Portfolio() {
   return (
     <header className="App-portfolio">
       <h1>Portfolio</h1>
+      <AiBot />
     </header>
+  );
+}
+
+function AiBot() {
+  const [aiQuest, setAiQuest] = useState("");
+  const aiReadySubmit = aiQuest !== "";
+  function handleInputChange(e) {
+    setAiQuest(e.target.value);
+  }
+  return (
+    <div className="ai-section">
+      <h2>AI powered Chat Bot</h2>
+      <div className="ai-prompt-section">
+        <form name="userForm">
+          <input
+            className="aiInput"
+            type="text"
+            name="userInput"
+            placeholder="Enter a prompt here"
+            onChange={handleInputChange}
+          />
+        </form>
+        <div className={aiReadySubmit ? "ai-buttons-ready" : "ai-buttons"}>
+          <img className="ai-image" src={rightArrow} alt="right-arrow" />
+          <img className="ai-audio" src={rightArrow} alt="right-arrow" />
+          {aiReadySubmit && (
+            <img className="ai-answer" src={rightArrow} alt="right-arrow" />
+          )}
+        </div>
+      </div>
+    </div>
   );
 }
