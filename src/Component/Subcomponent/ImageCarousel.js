851a3293b29bcedad3b2b61f69869a5c4175092d
msoukhrati <soukhratimarouane@gmail.com>
2024-06-11 15:23:41 +0200

    responsive bedouiage

--- src/Component/Subcomponent/ImageCarousel.js
+++ src/Component/Subcomponent/ImageCarousel.js
@@ -34,6 +34,7 @@ export default function ImageCarousel({ imgList, imgTitleList, imgStyleList }) {
       <div className={imgStyleList[3]}>
         {imgList.map((e, index) => (
           <motion.div
+            key={e+index}
             whileHover={{ scale: 1.5 }}
             className={index === imgIndex ? "activeswitchDot" : "switchDot"}
             onClick={() => setImgIndex(index)}
