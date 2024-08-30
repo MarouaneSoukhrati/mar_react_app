4bfcd3e48b482dfdc5e6507ba2cc09b31b697d21
Marouane Soukhrati <83184715+MarouaneSoukhrati@users.noreply.github.com>
2024-08-30 14:44:13 +0000

    openai addons

---
+++ src/Component/ChatBot/ChatBot.js
@@ -0,0 +1,8 @@
+import OpenAI from "openai";
+
+const OPENAI_API_KEY = process.env.REACT_APP_OPENAI;
+
+const openai = new OpenAI({
+  organization: "org-HLfIFoX452P8MKAwfhbfRvRx",
+  project: "proj_wZ4qMZ8wZmxaFd1c52xb1lec",
+});
