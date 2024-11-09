e0604dcb71dd38fd1b24a7f3c4c79b57adb566d8
Marouane Soukhrati <83184715+MarouaneSoukhrati@users.noreply.github.com>
2024-11-09 20:42:23 +0000

    dumb graphic

--- src/Component/Crypto/Crypto.js
+++ src/Component/Crypto/Crypto.js
@@ -1,7 +1,8 @@
 import "../../ComponentStyle/CryptoStyle/Crypto.css";
 
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef, memo } from "react";
 import { motion } from "framer-motion";
+import Chart from "chart.js/auto";
 
 import CreditCardLogo from "../../Logos/MoorLogo.svg";
 
@@ -37,45 +38,146 @@ export default function Crypto() {
   );
 }
 
-function Coin({ CoinName, CoinValue, CoinLogo }) {
+function Coin({ CoinName, CoinValue, CoinLogo, isSelected }) {
   return (
-    <div className="coinGr">
-      <img className="coinLogo" src={CoinLogo} alt="coinLogo">
-        {Coin.Logo}
-      </img>
-      <div className="coinName">{CoinName}</div>
-      <div className="coinValue">{CoinValue}</div>
+    <>
+      {isSelected && (
+        <motion.div
+          className="coinGrSelected"
+          animate={{
+            scale: [1, 1.1, 1.1, 1, 1],
+            rotate: [0, 0, 360, 360, 0],
+          }}
+          transition={{
+            duration: 3,
+            ease: "easeInOut",
+            times: [0, 0.2, 0.5, 0.8, 1],
+            repeat: Infinity,
+            repeatDelay: 1,
+          }}
+        >
+          <img className="coinLogo" src={CoinLogo} alt="coinLogo">
+            {Coin.Logo}
+          </img>
+          <div className="coinName">{CoinName}</div>
+          <div className="coinValueSelected">{CoinValue}</div>
+        </motion.div>
+      )}
+      {!isSelected && (
+        <div className="coinGr">
+          <img className="coinLogo" src={CoinLogo} alt="coinLogo">
+            {Coin.Logo}
+          </img>
+          <div className="coinName">{CoinName}</div>
+          <div className="coinValue">{CoinValue}</div>
+        </div>
+      )}
+    </>
+  );
+}
+
+function MyChart({ chartList, selectedIndex }) {
+  const chartRef = useRef(null);
+
+  useEffect(() => {
+    const ctx = chartRef.current.getContext("2d");
+
+    const myChart = new Chart(ctx, {
+      type: "line",
+      data: {
+        labels: chartList.map((e, index) => e.name),
+        datasets: [
+          {
+            label: "Coin Price",
+            data: chartList.map((e) => e.price),
+            backgroundColor: chartList.map((e, index) =>
+              index === selectedIndex ? "yellow" : "red"
+            ),
+            borderColor: "white",
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+          x: {
+            ticks: {
+              color: chartList.map((e, index) =>
+                index === selectedIndex ? "yellow" : "white"
+              ),
+              padding: 10,
+            },
+          },
+        },
+        elements: {
+          point: {
+            radius: chartList.map((e, index) =>
+              index === selectedIndex ? 7 : 3
+            ), // Adjust the point radius here
+          },
+        },
+      },
+    });
+
+    return () => {
+      myChart.destroy();
+    };
+  }, [chartList]);
+
+  return (
+    <div className="coinsChart">
+      <canvas ref={chartRef} />
     </div>
   );
 }
 
 function CoinGraphics() {
-  const options = {
-    headers: {
-      "x-access-token":
-        "coinranking0932477bb045d38988b7564f24af2967be9ffbc7e5bf9799",
-    },
-  };
   const [graphicsList, setGraphicsList] = useState([]);
+  const [sCoinIndex, setSCoinIndex] = useState(0);
   const [coinVName, setCoinVName] = useState("");
-
-  useEffect(() => {
-    fetch("https://api.coinranking.com/v2/coins", options)
-      .then((response) => response.json())
-      .then((result) => setGraphicsList(result.data.coins));
-  }, []);
+  const [responseValid, setResponseValid] = useState(false);
 
   let filteredGraphicsList = graphicsList.filter((el) =>
-    el.name.toUpperCase().startsWith(coinVName.toUpperCase())
+    el.name.toUpperCase().includes(coinVName.toUpperCase())
   );
 
-  let graphicsDes = filteredGraphicsList.map((e) => (
-    <Coin CoinName={e.name} CoinValue={e.price + "$"} CoinLogo={e.iconUrl} />
+  let graphicsDes = filteredGraphicsList.map((e, index) => (
+    <Coin
+      CoinName={e.name}
+      CoinValue={e.price + "$"}
+      CoinLogo={e.iconUrl}
+      isSelected={index === sCoinIndex}
+    />
   ));
 
   function handleChange(e) {
     setCoinVName(e.target.value);
   }
+
+  useEffect(() => {
+    const options = {
+      headers: {
+        "x-access-token":
+          "coinranking0932477bb045d38988b7564f24af2967be9ffbc7e5bf9799",
+      },
+    };
+    fetch("https://api.coinranking.com/v2/coins", options)
+      .then((response) => response.json())
+      .then((result) => {
+        setGraphicsList(result.data.coins);
+        setResponseValid(true);
+      });
+    const intervalId = setInterval(() => {
+      setSCoinIndex(Math.floor(Math.random() * filteredGraphicsList.length));
+    }, 3000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [filteredGraphicsList.length]);
 
   return (
     <div className="GraphicsBackGround">
@@ -87,7 +189,29 @@ function CoinGraphics() {
         placeholder="Search Coin name"
         onChange={handleChange}
       ></input>
-      <div>{graphicsDes}</div>
+      <MyChart chartList={filteredGraphicsList} selectedIndex={sCoinIndex} />
+      {responseValid && <div>{graphicsDes}</div>}
+      {!responseValid && (
+        <motion.div
+          animate={{
+            scale: [1, 2, 2, 1, 1],
+            rotate: [0, 0, 360, 360, 0],
+            borderRadius: ["20%", "20%", "50%", "50%", "20%"],
+          }}
+          transition={{
+            duration: 2,
+            ease: "easeInOut",
+            times: [0, 0.2, 0.5, 0.8, 1],
+            repeat: Infinity,
+            repeatDelay: 1,
+          }}
+          style={{
+            padding: "7vh",
+          }}
+        >
+          Loading
+        </motion.div>
+      )}
     </div>
   );
 }
