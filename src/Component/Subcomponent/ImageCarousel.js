78ef9061367e8137f768b4b0d7d4ae21147ae4c1
Marouane Soukhrati <83184715+MarouaneSoukhrati@users.noreply.github.com>
2026-08-20 01:38:54 +0100

    Update ImageCarousel.js

--- src/Component/Subcomponent/ImageCarousel.js
+++ src/Component/Subcomponent/ImageCarousel.js
@@ -19,7 +19,7 @@ export default function ImageCarousel({ imgList, imgTitleList, imgStyleList }) {
           alt="left-arrow"
           whileHover={{ scale: 2.2 }}
           whileTap={{ scale: 2.2 }}
-          onClick={() =>
+          onClick={(imgIndex) =>
             setImgIndex(imgIndex === 0 ? imgList.length - 1 : imgIndex - 1)
           }
         />
