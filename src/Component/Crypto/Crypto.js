fd08b2f2d1e96faaba00bedc63437aeb139ced9b
Marouane Soukhrati <83184715+MarouaneSoukhrati@users.noreply.github.com>
2026-08-23 18:18:15 +0100

    Update Crypto.js

--- src/Component/Crypto/Crypto.js
+++ src/Component/Crypto/Crypto.js
@@ -13,7 +13,7 @@ function InfiniteScrollFramer() {
     <div style={{ overflow: "hidden", width: "100%", display: "flex" }}>
       <motion.div
         style={{ display: "flex", gap: "1rem" }}
-        animate={{ x: ["0%", "-100%"] }}
+        animate={{ x: ["0%", "-50%"] }}
         transition={{
           ease: "linear",
           duration: 10,
@@ -21,21 +21,7 @@ function InfiniteScrollFramer() {
         }}
       >
         {/* Render the list twice to create the seamless loop */}
-        {[...cards].map((card, index) => (
-        <CryptoCard key={index}/>
-        ))}
-      </motion.div>
-      <motion.div
-        style={{ display: "flex", gap: "1rem" }}
-        animate={{ x: ["0%", "-100%"] }}
-        transition={{
-          ease: "linear",
-          duration: 10,
-          repeat: Infinity,
-        }}
-      >
-        {/* Render the list twice to create the seamless loop */}
-        {[...cards].map((card, index) => (
+        {[...cards, ...cards].map((card, index) => (
         <CryptoCard key={index}/>
         ))}
       </motion.div>
