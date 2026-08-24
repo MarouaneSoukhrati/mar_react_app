47c81f5bec05103596d8f6e64e6abe95ba80b626
Marouane Soukhrati <83184715+MarouaneSoukhrati@users.noreply.github.com>
2026-08-24 18:30:54 +0100

    Update Crypto.js

--- src/Component/Crypto/Crypto.js
+++ src/Component/Crypto/Crypto.js
@@ -10,7 +10,7 @@ function InfiniteScrollFramer() {
   return (
     <div style={{ overflowX: "auto", width: "100%", display: "flex" }}>
       <motion.div
-        style={{ display: "flex", gap: "1rem", paddingRight: "1rem" }}
+        style={{ display: "flex" }}
         animate={{ x: ["0%", "-100%"] }}
         transition={{
           ease: "linear",
@@ -24,7 +24,7 @@ function InfiniteScrollFramer() {
       </motion.div>
       
        <motion.div
-        style={{ display: "flex", gap: "1rem", paddingRight: "1rem" }}
+        style={{ display: "flex" }}
         animate={{ x: ["0%", "-100%"] }}
         transition={{
           ease: "linear",
