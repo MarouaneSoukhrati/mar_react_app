edae3119fffe6afe5383ee3ccebf6a13bf856b0e
Marouane Soukhrati <83184715+MarouaneSoukhrati@users.noreply.github.com>
2026-08-24 18:56:34 +0100

    Update Marquee.js

--- src/Component/Subcomponent/Marquee.js
+++ src/Component/Subcomponent/Marquee.js
@@ -2,6 +2,41 @@ import "../../ComponentStyle/SubcomponentStyle/Marquee.css";
 import { useState } from "react";
 import { motion } from "framer-motion";
 
+function InfiniteScrollFramer() {
+  return (
+    <div style={{ overflowX: "auto", width: "100%", display: "flex" }}>
+      <motion.div
+        style={{ display: "flex" }}
+        animate={{ x: ["0%", "-100%"] }}
+        transition={{
+          ease: "linear",
+          duration: 10,
+          repeat: Infinity,
+        }}
+      >
+        {[1, 2, 3].map((card, index) => (
+        <CryptoCard key={index}/>
+        ))}
+      </motion.div>
+      
+       <motion.div
+        style={{ display: "flex" }}
+        animate={{ x: ["0%", "-100%"] }}
+        transition={{
+          ease: "linear",
+          duration: 10,
+          repeat: Infinity,
+        }}
+      >
+        {[1, 2, 3].map((card, index) => (
+        <CryptoCard key={index}/>
+        ))}
+      </motion.div>
+    </div>
+  );
+}
+
+
 export default function Marquee({ imgList }) {
   return (
     <div className="Marquee">
