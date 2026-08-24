59e6338d8debe180a6a4fa45c6177b5c2fa07050
Marouane Soukhrati <83184715+MarouaneSoukhrati@users.noreply.github.com>
2026-08-24 19:21:22 +0100

    Update Crypto.js

--- src/Component/Crypto/Crypto.js
+++ src/Component/Crypto/Crypto.js
@@ -4,7 +4,7 @@ import React, { useEffect, useState, useRef } from "react";
 import { motion } from "framer-motion";
 import Chart from "chart.js/auto";
 
-import Marquee from "../Subcomponent/Marquee";
+/*import Marquee from "../Subcomponent/Marquee";*/
 
 import CreditCardLogo from "../../Logos/MoorLogo.svg";
 
